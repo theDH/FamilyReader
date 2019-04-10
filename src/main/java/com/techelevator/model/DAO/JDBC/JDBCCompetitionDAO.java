1b291ff1b3c0ac0519f6bd67293c86766941eb86
Santhiya Mahilkanthan <santhiya.mahil123@gmail.com>
2019-04-10 11:03:22 -0400

    Update-Santhiya-Day3

--- src/main/java/com/techelevator/model/DAO/JDBC/JDBCCompetitionDAO.java
+++ src/main/java/com/techelevator/model/DAO/JDBC/JDBCCompetitionDAO.java
@@ -24,34 +24,43 @@ public class JDBCCompetitionDAO implements CompetitionDAO{
 
 	@Override
 	public void addPersonToCompetition(Competition newCompetition, long personId) {
-		// TODO Auto-generated method stub
 		String query="INSERT INTO competition_people (people_id, competition_id) VALUES (?,?)";
-		jdbcTemplate.update(query, personId, newCompetition.getCompetitionId());
+		jdbcTemplate.update(query, personId, extracted(newCompetition).getCompetitionId());
 	}
 
 	@Override
 	public void createNewCompetition(Competition newCompetition) {
-		// TODO Auto-generated method stub
-		String query="INSERT INTO competition (competition_id, name_of_competition, start_date, end_date, description, minutes_to_finish) VALUES (?,?,?,?,?,?)";
-		jdbcTemplate.update(query,newCompetition.getCompetitionId(),newCompetition.getNameOfCompetition(),newCompetition.getStartDate(),newCompetition.getEndDate(),newCompetition.getDescription(),newCompetition.getMinutesToFinish());
-		
+		String insertNewCompetition="INSERT INTO competition (competition_id, name_of_competition, start_date, end_date, description, minutes_to_finish) VALUES (?,?,?,?,?,?)";
+		jdbcTemplate.update(insertNewCompetition,extracted(newCompetition).getCompetitionId(),extracted(newCompetition).getNameOfCompetition(),extracted(newCompetition).getStartDate(),extracted(newCompetition).getEndDate(),extracted(newCompetition).getDescription(),extracted(newCompetition).getMinutesToFinish());
 	}
 
 	@Override
 	public List<Competition> getListOfFinishedCompetitions(LocalDate todayDate) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Competition> finishedCompetitions = new ArrayList<Competition>();
+		String searchForFinishedCompetitions= "SELECT * FROM competition WHERE end_date < ?";
+		SqlRowSet resutls=jdbcTemplate.queryForRowSet(searchForFinishedCompetitions, todayDate);
+		while (results.next()) {
+			Site competition = mapRowToCompetition(results);
+			finishedCompetitions.add(competition);
+		}
+		return finishedCompetitions;
 	}
 
 	@Override
 	public List<Competition> getListOfActiveCompetitions(LocalDate todayDate) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Competition> activeCompetitions = new ArrayList<Competition>();
+		String getSqlOfActiveCompetitions = "SELECT * FROM competition WHERE ? BETWEEN start_date AND end_date";
+		SqlRowSet results=jdbcTemplate.queryForRowSet(getSqlOfActiveCompetitions, todayDate);
+		while (results.next()) {
+			Site competition = mapRowToCompetition(results);
+			activeCompetitions.add(competition);
+		}
+		return activeCompetitions;
 	}
 
 	@Override
 	public List<Person> getListOfPeopleInCompetition(long competitionId) {
-		// TODO Auto-generated method stub
+	
 		return null;
 	}
 
@@ -63,8 +72,18 @@ public class JDBCCompetitionDAO implements CompetitionDAO{
 
 	@Override
 	public Competition updateCompetition(Competition competition) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	private Competition mapRowToCompetition(SqlRowSet results) {
+		Competition competition = new Competition();
+		competition.setCompetitionId(results.getlong("competition_id"))
+		competition.setNameOfCompetition(results.getString("name_of_competition"));
+		competition.setStartDate(results.getLocalDate("start_date"));
+		competition.setEndDate(results.getLocalDate("end_date"));
+		competition.setDescription(results.getString("description"));
+		competition.setMinutesToFinish(results.getInt("minutes_to_finish"));
+		return competition;
+	}
 }
