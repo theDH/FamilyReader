965a28a563babfcb2eb0a0c749197bd8a62ddf4d
Steven Markel <smarkel@gmail.com>
2019-04-11 09:03:53 -0400

    screwing things up

--- vuereaderapp/src/components/PrimaryButton.vue
+++ vuereaderapp/src/components/PrimaryButton.vue
@@ -1,9 +1,5 @@
 <template>
 <div>
-  <b-button>Button</b-button>
-  <b-button variant="danger">Button</b-button>
-  <b-button variant="success">Button</b-button>
-  <b-button variant="outline-primary">Button</b-button>
   <button class="Button">Button</button>
 </div>
   
