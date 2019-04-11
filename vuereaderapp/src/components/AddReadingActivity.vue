92f66f91bbbbd2188c9133f0fa0129f41c0d846c
Alexandra Gales <agales136@gmail.com>
2019-04-11 12:22:50 -0400

    incomplete components add/family

--- vuereaderapp/src/components/AddReadingActivity.vue
+++ vuereaderapp/src/components/AddReadingActivity.vue
@@ -2,18 +2,22 @@
   <div class = "add-reading-activity">
     <h1>Add Reading Activity</h1>
     <ul>
-      <li>v-for="activity in activities" v-bind:key ="activity">{{activity.}}</li>
+      <li>v-for="activity in activities" v-bind:key ="activity">{{activity.session}}</li>
     </ul>
     <primary-button/>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import PrimaryButton from './PrimaryButton'
 export default {
+  name:'AddReadingActivity',
   data () {
     return {
-    
+        activities:[
+            {session:'cheese'}
+        ]
       
     }
   },
