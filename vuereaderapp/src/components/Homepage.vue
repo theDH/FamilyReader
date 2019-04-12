355def28a5f3f5b3ade447a0c05eb01bc6933f53
Steven Markel <smarkel@gmail.com>
2019-04-12 13:51:02 -0400

    homepage

---
+++ vuereaderapp/src/components/Homepage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <family-list id="familyList"/>
+    <book-list id="bookList"/>
+  </div>
+</template>
+
+<script>
+import FamilyList from './FamilyList'
+import BookList from './BookList'
+export default {
+  components: {
+    FamilyList,
+    BookList
+  }
+
+}
+</script>
+
+<style>
+
+</style>
