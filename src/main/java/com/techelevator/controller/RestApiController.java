9b8a6db28bad59afc201636d56d9936e25a138d9
Steven Markel <smarkel@gmail.com>
2019-04-14 12:17:38 -0400

    competitionlist

--- src/main/java/com/techelevator/controller/RestApiController.java
+++ src/main/java/com/techelevator/controller/RestApiController.java
@@ -103,15 +103,18 @@ public class RestApiController {
 	
 	@CrossOrigin(origins = "http://localhost:8081")
 	@RequestMapping(path="/competitionlistperson", method=RequestMethod.GET)
-	public List<Competition> getListOfActiveCompetitions(@RequestParam LocalDate todayDate, long familyId) {
-		return competitionDAO.getListOfActiveCompetitions(todayDate, familyId);
+	public List<Competition> getListOfActiveCompetitionsByPerson(long personId) {
+		LocalDate todayDate = LocalDate.now();
+		return competitionDAO.getListOfCompetitionsByPerson(todayDate, personId);
 	}
 	
 	@CrossOrigin(origins = "http://localhost:8081")
 	@RequestMapping(path="/competitionlistfamily", method=RequestMethod.GET)
-	public List<Person> getListOfPeopleInCompetition(@RequestParam long competitionId) {
-		return competitionDAO.getListOfPeopleInCompetition(competitionId);
+	public List<Competition> getListOfActiveCompetitionsByFamily(@RequestParam long familyId) {
+		LocalDate today = LocalDate.now();
+		return competitionDAO.getListOfActiveCompetitionsByFamily(today, familyId);
 	}
+	
 	@CrossOrigin(origins = "http://localhost:8081")
 	@RequestMapping(path="/homepage", method=RequestMethod.GET)
 	public void homepage() {
