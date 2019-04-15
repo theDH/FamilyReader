cea67fe14d3f056f0fb1be637184585b5c582cce
Alexandra Gales <agales136@gmail.com>
2019-04-14 21:00:48 -0400

    addcomp updates

--- vuereaderapp/src/components/AddCompetition.vue
+++ vuereaderapp/src/components/AddCompetition.vue
@@ -1,11 +1,19 @@
 <template>
-    
+  <div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import PrimaryButton from './PrimaryButton'
+import EventBus from './EventBus'
 export default {
-
+name: 'AddCompetition',
+  data () {
+  return {
+    familyId: this.$session.get('familyId'),
+    }
+  }
 }
 </script>
 
