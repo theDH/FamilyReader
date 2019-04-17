ebd38b6a455637ac5e2794924e467686781844c6
Santhiya Mahilkanthan <santhiya.mahil123@gmail.com>
2019-04-17 15:47:40 -0400

    update-app.vue

--- vuereaderapp/src/App.vue
+++ vuereaderapp/src/App.vue
@@ -9,15 +9,13 @@
         text-variant="white"
         >
           <v-card-text>
-            Remember  <a target="_blank" href="https://www.bookitprogram.com/">Book-It</a>? This is like that but without Pizza Hut.
-            <br>
-            This application creates fun ways to manage and encourage reading within the family.  Using Booknanza, parents are able to track both their own reading progress and that of their child. 
-            Books can be added by searching for the title, author or ISBN. Prizes can be set by the parents that can be accessed by the children upon completing a reading goal. In addition to individual reading activities, parents can set up family competitions to encourage group progress. Booknanza allows parents to track the activity of each child, add/delete goals and competitions, and create unique prizes.
+            Remember<a target="_blank" href="https://www.bookitprogram.com/">Book-It</a>? This is like that but without Pizza Hut.
+            <br>This application creates fun ways to manage and encourage reading within the family.Using Booknanza, parents are able to track both their own reading progress and that of their child.Books can be added by searching for the title, author or ISBN. Prizes can be set by the parents that can be accessed by the children upon completing a reading goal. In addition to individual reading activities, parents can set up family competitions to encourage group progress. Booknanza allows parents to track the activity of each child, add/delete goals and competitions, and create unique prizes.
           </v-card-text>
         </v-card>
       </div>
       <v-spacer></v-spacer>
-      <h3>You are currently viewing: {{ navigation }}</h3> 
+      <h3>You are currently viewing: {{ navigation }}</h3>
       <v-content>
         <router-view/>
         <reader-footer></reader-footer>
